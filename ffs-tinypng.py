85173f32b0739753babc373aa73d2487ba46673e
iLearner <ilearner.432@gmail.com>
2021-09-17 17:18:10 +0200

    refactor: code refactoring

--- ffs-tinypng.py
+++ ffs-tinypng.py
@@ -32,11 +32,11 @@ async def optimize(image_path: str):
 
 
 # look up the specified path and count, find and allocate files. optionally also only get files with a certain size
-async def lookUp(path, min_size=None):
+async def look_up(path, min_size=None):
     # initialize the counter at 0
-    totalCount = 0
+    total_count = 0
     # create an empty list initialized at 0
-    extensionCount = [0] * len(extensions)
+    extension_count = [0] * len(extensions)
     # extensions string
     found_extensions = ""
     # all the found files (their paths)
@@ -47,23 +47,17 @@ async def lookUp(path, min_size=None):
         for file_path in Path(path).rglob(extension):
 
             # if min size isnt provided return all of them
-            if min_size is None:
-                totalCount += 1
-                extensionCount[i] += 1
-                files.append(file_path)
-
             # otherwise return the conditional files
-            else:
-                if Path(file_path).stat().st_size > min_size:
-                    totalCount += 1
-                    extensionCount[i] += 1
-                    files.append(file_path)
+            if min_size is None or (min_size is not None and Path(file_path).stat().st_size > min_size):
+                total_count += 1
+                extension_count[i] += 1
+                files.append(file_path)
 
     # populate the extensions string
     for i, extension in enumerate(extensions):
-        found_extensions += f"{extension}: {extensionCount[i]} | "
+        found_extensions += f"{extension}: {extension_count[i]} | "
 
-    return totalCount, found_extensions, files
+    return total_count, found_extensions, files
 
 
 async def main():
@@ -77,45 +71,44 @@ async def main():
 
     # get total files found, extended files and file paths in specified path
     print(f"Looking up '{path}' for {extensions}...")
-    totalCount, found_extensions, files = await lookUp(path)
-    print(f"Look up complete! Found a total of {totalCount} files. | {found_extensions[:-2].replace('*.', '')}")
+    total_count, found_extensions, files = await look_up(path)
+    print(f"Look up complete! Found a total of {total_count} files. | {found_extensions[:-2].replace('*.', '')}")
 
     # start the compression
     print(f"\nStarting compression..")
     for i, file_path in enumerate(files):
-        sys.stdout.write(f"\rCompressed {i} of {totalCount} images | {round(i / totalCount * 100, 2)}% | Compressing file: {file_path}")
+        sys.stdout.write(f"\rCompressed {i} of {total_count} images | {round(i / total_count * 100, 2)}% | Compressing file: {file_path}")
         await optimize(file_path)
 
-    sys.stdout.write(f"\rCompressed {totalCount} of {totalCount} images | 100%")
+    sys.stdout.write(f"\rCompressed {total_count} of {total_count} images | 100%")
     sys.stdout.flush()
     # compression finished! Now check if there are still any files over 2mb
 
     # loop through the amount of times we can attempt to compress
     for j in range(MAX_COMPRESSING_ATTEMPTS + 1):
-
         # get all files above MAX_FILE_SIZE
-        totalCount, found_extensions, files = await lookUp(path, MAX_FILE_SIZE*1000000)
+        total_count, found_extensions, files = await look_up(path, MAX_FILE_SIZE*1000000)
 
         # do we have at least one file to compress?
-        if totalCount > 0:
+        if total_count > 0:
 
             if j == 0:
-                print(f"\nAdditional compression required: {totalCount} files found above {MAX_FILE_SIZE}M limit!")
+                print(f"\nAdditional compression required: {total_count} files found above {MAX_FILE_SIZE}M limit!")
 
             # do we still have compressing attempts?
             if j < MAX_COMPRESSING_ATTEMPTS:
 
                 # alright lets start compressing..
-                print(f"\nFound {totalCount} more files to compress.. compressing (Attempt {j+1} of {MAX_COMPRESSING_ATTEMPTS})")
+                print(f"\nFound {total_count} more files to compress.. compressing (Attempt {j+1} of {MAX_COMPRESSING_ATTEMPTS})")
                 for i, file_path in enumerate(files):
-                    sys.stdout.write(f"\rCompressed {i} of {totalCount} images | {round(i / totalCount * 100, 2)}% | Compressing file: {file_path}")
+                    sys.stdout.write(f"\rCompressed {i} of {total_count} images | {round(i / total_count * 100, 2)}% | Compressing file: {file_path}")
                     await optimize(file_path)
-                sys.stdout.write(f"\rCompressed {totalCount} of {totalCount} images | 100%")
+                sys.stdout.write(f"\rCompressed {total_count} of {total_count} images | 100%")
                 sys.stdout.flush()
             else:
 
                 # more files to compress were found but we have no more compressing attempts left :/
-                print(f"\nAttention! {totalCount} more files to compress found but out of attempts ({MAX_COMPRESSING_ATTEMPTS})\n")
+                print(f"\nAttention! {total_count} more files to compress found but out of attempts ({MAX_COMPRESSING_ATTEMPTS})\n")
                 print(f"Following files were compressed yet their size is greater than {MAX_FILE_SIZE}M:\n")
                 for file_path in files:
                     print(f"{file_path}\n")
